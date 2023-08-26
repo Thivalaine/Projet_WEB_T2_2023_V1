8b9306ff1b36d51411aaf1e0bdc02723975b232d
MercoDix <nathandavigny20@gmail.com>
2023-08-26 15:30:50 +0200

    fetch script.js

--- script.js
+++ script.js
@@ -2,7 +2,23 @@
 window.addEventListener("load", init);
 
 function init(){
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    let map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+    }).addTo(map);
 }
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+async function fetchData() {
+
+    const response = await fetch('http://localhost:3000/features');
+    const data = await response.json();
+    let lieu = document.getElementById("lieu");
+    lieu.innerText = data[0].properties.name;
+    console.log(data[0].properties.name);
+
+    return data;
+}
+
+fetchData();
+
